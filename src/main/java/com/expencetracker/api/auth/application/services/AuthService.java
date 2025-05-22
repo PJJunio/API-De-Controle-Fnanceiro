e73317872bc39bb5bd4652fcf68dfc579eb9d00f
PJJunio <paulodjunior.dev@gmail.com>
2025-05-22 18:29:19 -0300

    Removidos todos os arquivos do repositório

--- src/main/java/com/expencetracker/api/auth/application/services/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.expencetracker.api.auth.application.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AuthService {
-}
-
-
--- src/main/resources/db/migration/V2__create-expenses-trable.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE expenses (
-    expenses_ids SERIAL PRIMARY KEY,
-    names VARCHAR (25) NOT NULL,
-    descriptions CHAR(255),
-    amounts NUMERIC(10, 2) NOT NULL,
-    categories VARCHAR(50) NOT NULL,
-    active BOOLEAN DEFAULT FALSE NOT NULL,
-    user_external_id UUID REFERENCES users(external_id)
-);
